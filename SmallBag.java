da0cabf8ee31dc9aa9f50d6c5f5405584d7da90d
BPowell2 <112312288+BPowell2@users.noreply.github.com>
2022-08-28 20:14:06 -0400

    Create SmallBag.java

---
+++ SmallBag.java
@@ -0,0 +1,13 @@
+//Benjamin Powell this is the smallbag generic class 0/27/22
+public class SmallBag<T> {
+    Item<T> item;
+
+    void setItem(Item Value) {
+    
+        item = Value;
+    }// end of setItem
+    Item getItem() {
+    
+        return item;
+    }//end of getItem 
+}//end of class 
