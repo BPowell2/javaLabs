c45aa3e38cb86b54cd2aabcf7da47da84ee63692
BPowell2 <112312288+BPowell2@users.noreply.github.com>
2022-09-12 13:10:56 -0400

    Update Item.java

--- Item.java
+++ Item.java
@@ -2,10 +2,19 @@
 public class Item<T> {
     private T e;
 
+    /**
+     * This method is used to set the value of e 
+     * @param Value is passed by the uder to set e
+     */
     void setE(T Value) {
     
         e = Value;
     }//end of setE
+    
+    /**
+     * this is a normal getter method to get the value of e
+     * @return the value of e 
+     */
     T getter() {
     
         return e;
