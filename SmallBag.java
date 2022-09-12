e99bed1a9e866f15ef339e16f569776b765eec4b
BPowell2 <112312288+BPowell2@users.noreply.github.com>
2022-09-12 13:11:31 -0400

    Update SmallBag.java

--- SmallBag.java
+++ SmallBag.java
@@ -1,11 +1,19 @@
-//Benjamin Powell this is the smallbag generic class 0/27/22
+//Benjamin Powell this is the smallbag generic class 08/27/22
 public class SmallBag<T> {
     Item<T> item;
-
+    
+    /**
+     * this is the setItem method that sets the item within the bag
+     * @param Value is the item that is added into the bag
+     */
     void setItem(Item Value) {
     
         item = Value;
     }// end of setItem
+    /**
+     * this is the normal get method for the item inside the bag
+     * @return the item inside the bag
+     */
     Item getItem() {
     
         return item;
